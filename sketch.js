241d1d1fc2e7f44b81e1ebc21c2e3e95a7af9102
jaymesdec <jaymesdec@gmail.com>
2019-08-29 19:39:14 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1000, 600);
   frameRate(3);
   textFont("Helvetica");
 }
@@ -13,7 +13,7 @@ function setup() {
 function draw() {
   background(220);
   textSize(80);
-  text("computational ", 25, 200);
+  text("Computational ", 25, 200);
   let profession = floor(random(listOfJobs.length));
   text(listOfJobs[profession], 25, 280);
 }
